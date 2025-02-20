a191dd3a665aa52289f11ea3aea9a995647d3733
Breno Melo <brenolucasmelo@gmail.com>
2025-02-20 13:07:07 -0300

    feat: card creation and home screen completion

---
+++ src/app/[slug]/components/consumption-method-option.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { ConsumptionMethod } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ConsumptionMethodOptionProps {
+  slug: string;
+  imageUrl: string;
+  imageAlt: string;
+  buttonText: string;
+  option: ConsumptionMethod;
+}
+
+const ConsumptionMethodOptionProps = ({
+  slug,
+  imageUrl,
+  imageAlt,
+  buttonText,
+  option,
+}: ConsumptionMethodOptionProps) => {
+  return (
+    <Card>
+      <CardContent className="grap-8 flex flex-col items-center py-8">
+        <div className="relative h-[80px] w-[80px]">
+          <Image
+            src={imageUrl}
+            fill
+            alt={imageAlt}
+            className="object-contain"
+          />
+        </div>
+        <Button variant="secondary" className="rounded-full">
+          <Link href={`/${slug}/menu?consumptionMethod=${option}`}>
+            {buttonText}
+          </Link>
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ConsumptionMethodOptionProps;
--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
 import Image from "next/image";
 import { notFound } from "next/navigation";
+import ConsumptionMethodOption from "./components/consumption-method-option";
 
 interface RestaurantPageProps {
   params: Promise<{ slug: string }>;
@@ -24,12 +25,30 @@ const RestaurantPage = async ({ params }: RestaurantPageProps) => {
         />
         <h2 className="font-semibold">{restaurant.name}</h2>
       </div>
+      {/* TEXTO INTRODUTÓRIO */}
       <div className="space-y-2 pt-24 text-center">
         <h3 className="text-2x1 font-semibold">Seja bem-vindo!</h3>
         <p className="opacity-55">
           Escolha como prefere aproveitar sua refeição. Estamos aqui para
           oferecer praticidade e sabor em cada detalhe!
         </p>
+      </div>
+      {/* CARDS */}
+      <div className="grid grid-cols-2 gap-4 pt-14">
+        <ConsumptionMethodOption
+          slug={slug}
+          option="DINE_IN"
+          imageUrl="/dine_in.png"
+          imageAlt="Comer aqui"
+          buttonText="Para comer aqui"
+        />
+        <ConsumptionMethodOption
+          slug={slug}
+          option="TAKEAWAY"
+          imageUrl="/takeaway.png"
+          imageAlt="Para levar"
+          buttonText="Para levar"
+        />
       </div>
     </div>
   );
